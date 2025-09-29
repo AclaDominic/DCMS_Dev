0dcc689ddb00449c2a7762b6c801f271898c5417
JoshuaBolilan <joshuabolilan003@gmail.com>
2025-09-29 23:11:26 +0800

    a

--- bend/config/cors.php
+++ bend/config/cors.php
@@ -23,7 +23,7 @@ return [
         'http://127.0.0.1:5173',
         'http://127.0.0.1:8000',
         'https://it-42-capstone.vercel.app',
-        'https://unparadoxal-seasonably-oren.ngrok-free.dev',
+        'https://postomental-miley-rangier.ngrok-free.dev ',
     ],
 
     'allowed_origins_patterns' => [],
